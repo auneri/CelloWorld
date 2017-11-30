526fd181dcb4bd38d27310f8d51c2e2b91ddcf3d
Ali Uneri <ali.uneri@jhu.edu>
2017-11-29 23:51:47 -0500

    Update Qt example to use Qt v5 (default) and v4 (fallback)

--- HelloQt/main.cxx
+++ HelloQt/main.cxx
@@ -1,8 +1,11 @@
+#include <iostream>
+
 #ifdef _WIN32
 #  pragma warning(push)
 #endif
 
 #include <QApplication>
+#include <QtGlobal>
 
 #include "window.h"
 
@@ -12,6 +15,13 @@
 
 
 int main(int argc, char *argv[]) {
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
+  std::cout << "Using Qt < v5" << std::endl;
+#else
+  std::cout << "Using Qt >= v5" << std::endl;
+#endif
+
   QApplication application(argc, argv);
   Window window;
   window.show();
