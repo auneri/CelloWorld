15f2a22cbfd512858be1f93638eeb578be26173a
Ali Uneri <ali.uneri@jhu.edu>
2024-12-10 00:05:43 -0500

    Remove deprecated function calls

--- HelloPythonCXX/main.cxx
+++ HelloPythonCXX/main.cxx
@@ -12,18 +12,32 @@ int main(int, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  Py_SetProgramName(program);
-  Py_Initialize();
+  PyStatus status;
+  PyConfig config;
+  PyConfig_InitPythonConfig(&config);
+
+  status = PyConfig_SetString(&config, &config.program_name, program);
+  if (PyStatus_Exception(status)) {
+    PyConfig_Clear(&config);
+    PyMem_RawFree(program);
+    std::cerr << "PyConfig_SetString failed" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  status = Py_InitializeFromConfig(&config);
+  PyConfig_Clear(&config);
+  if (PyStatus_Exception(status)) {
+    std::cerr << "Py_InitializeFromConfig failed" << std::endl;
+    PyMem_RawFree(program);
+    return EXIT_FAILURE;
+  }
+
   PyRun_SimpleString("print('hello, world')\n");
 
-#if PY_MAJOR_VERSION >= 3 && PY_MINOR_VERSION >= 6
   if (Py_FinalizeEx() < 0) {
     std::cerr << "Py_FinalizeEx failed" << std::endl;
     return EXIT_FAILURE;
   }
-#else
-  Py_Finalize();
-#endif
 
   PyMem_RawFree(program);
 
